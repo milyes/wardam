028d3c3f6b60b76603c0890ec05a776b5c541250
zoubirou ilyes <119434103+milyes@users.noreply.github.com>
2024-12-30 10:44:30 -0500

    Update UserController.js

---
+++ MonProjet/backend/controllers/UserController.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// Route pour créer un utilisateur
+router.post('/users', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Route pour récupérer tous les utilisateurs
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
