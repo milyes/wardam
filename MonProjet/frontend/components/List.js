1b0b97feaa0e3e53e32cac695577e3f6f135b27a
zoubirou ilyes <119434103+milyes@users.noreply.github.com>
2024-12-30 10:41:28 -0500

    Update List.js

---
+++ MonProjet/frontend/components/List.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const List = ({ users }) => {
+  return (
+    <ul>
+      {users.map((user, index) => (
+        <li key={index}>{user}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default List;
